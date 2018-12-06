91dc237e3142c6cab84196b550352c5631f73ede
Vinay Prasad <vinayprasad.inbox@gmail.com>
2018-12-05 22:05:47 -0500

    fat jar

---
+++ src/main/java/com/handsoncoder/demo/MyClass.java
@@ -0,0 +1,11 @@
+package com.handsoncoder.demo;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from main method");
+		for (String arg : args) {
+			System.out.println("Argumnet - "+ arg );
+		}
+	}
+}
